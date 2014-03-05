c460a47fb22b12e20de139aee47bda2120d278aa
DFEFred <fredericfrancisit@gmail.com>
2014-03-05 15:23:08 +0100

    Test App First Commit

---
+++ TestApp/TestApp/Function/PropFonctions.cs
@@ -0,0 +1,19 @@
+﻿using DFEImmobilier.Fenetre;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFEImmobilier.Function
+{
+    class PropFonctions
+    {
+        public static void AfficheGoogleSearch(string adress){
+            Streetview view = new Streetview();
+            view.Show();
+            Uri uri = new Uri(@adress);
+            view.Browser.Navigate(uri);
+           }
+    }
+}
---
+++ TestApp/TestApp/propList.cs
@@ -0,0 +1,15 @@
+﻿using DFEImmobilier.DB;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DFEImmobilier
+{
+    public class propList : ObservableCollection<tbl_Proprio>
+    {
+    }
+}
